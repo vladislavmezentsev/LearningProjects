cab8c3b1d0b32a75839700a75119f22a5dc6487a
Vladislav Mezentsev <vleji@yandex.ru>
2022-11-26 00:16:41 +0300

    123

--- FilesAndNetwork/DataCollector/src/DataCollector.java
+++ FilesAndNetwork/DataCollector/src/DataCollector.java
@@ -67,7 +67,6 @@ public class DataCollector {
                 String depth = String.valueOf(stationJsonObject.get("depth_meters"));
                 listStations.get(stationName).setDepth(depth);
             }
-
         }
     }
 
