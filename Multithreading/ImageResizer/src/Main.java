babf240bab29784ae3b07f764a9a569754db87bb
Vladislav Mezentsev <vleji@yandex.ru>
2022-12-06 00:07:51 +0300

    Homework 17.2

--- Multithreading/ImageResizer/src/Main.java
+++ Multithreading/ImageResizer/src/Main.java
@@ -3,10 +3,11 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class Main {
+    private static int newWidth = 300;
 
     public static void main(String[] args) {
-        String srcFolder = "/users/sortedmap/Desktop/src";
-        String dstFolder = "/users/sortedmap/Desktop/dst";
+        String srcFolder = "\\users\\User\\Desktop\\src";
+        String dstFolder = "\\users\\User\\Desktop\\dst";
 
         File srcDir = new File(srcFolder);
 
@@ -14,38 +15,40 @@ public class Main {
 
         File[] files = srcDir.listFiles();
 
-        try {
-            for (File file : files) {
-                BufferedImage image = ImageIO.read(file);
-                if (image == null) {
-                    continue;
-                }
+        int oneThird = files.length / 3;
+        int firstPart = oneThird / 2;
+        int secondPart = oneThird - firstPart;
 
-                int newWidth = 300;
-                int newHeight = (int) Math.round(
-                    image.getHeight() / (image.getWidth() / (double) newWidth)
-                );
-                BufferedImage newImage = new BufferedImage(
-                    newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-                );
 
-                int widthStep = image.getWidth() / newWidth;
-                int heightStep = image.getHeight() / newHeight;
+        File[] files1 = new File[firstPart];
+        System.arraycopy(files, 0, files1, 0, firstPart);
+        ImageResizer resizer1 = new ImageResizer(files1, newWidth, dstFolder, start);
+        resizer1.start();
 
-                for (int x = 0; x < newWidth; x++) {
-                    for (int y = 0; y < newHeight; y++) {
-                        int rgb = image.getRGB(x * widthStep, y * heightStep);
-                        newImage.setRGB(x, y, rgb);
-                    }
-                }
+        File[] files2 = new File[secondPart];
+        System.arraycopy(files, 0, files2, 0, secondPart);
+        ImageResizer resizer2 = new ImageResizer(files2, newWidth, dstFolder, start);
+        resizer2.start();
 
-                File newFile = new File(dstFolder + "/" + file.getName());
-                ImageIO.write(newImage, "jpg", newFile);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        File[] files3 = new File[firstPart];
+        System.arraycopy(files, 0, files3, 0, firstPart);
+        ImageResizer resizer3 = new ImageResizer(files3, newWidth, dstFolder, start);
+        resizer3.start();
 
-        System.out.println("Duration: " + (System.currentTimeMillis() - start));
+        File[] files4 = new File[secondPart];
+        System.arraycopy(files, 0, files4, 0, secondPart);
+        ImageResizer resizer4 = new ImageResizer(files4, newWidth, dstFolder, start);
+        resizer4.start();
+
+        File[] files5 = new File[firstPart];
+        System.arraycopy(files, 0, files5, 0, firstPart);
+        ImageResizer resizer5 = new ImageResizer(files5, newWidth, dstFolder, start);
+        resizer5.start();
+
+        File[] files6 = new File[secondPart];
+        System.arraycopy(files, 0, files6, 0, secondPart);
+        ImageResizer resizer6 = new ImageResizer(files6, newWidth, dstFolder, start);
+        resizer6.start();
+
     }
 }
